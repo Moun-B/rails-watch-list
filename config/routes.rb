bf02449e86212b5b553996bd1da063a555fe328f
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-08-11 13:18:07 +0900

    finished basic controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :destroy]
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmark, only: [:destroy]
 end
